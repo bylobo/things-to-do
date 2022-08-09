b2faf4ab6c7a52fc0cae8395e786f056971a2650
Bylobo <carloamarin16a6@gmail.com>
2022-08-09 13:34:52 -0400

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ListaDeTareas from "./components/ListaDeTareas";
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className="tareas-lista-principal">
+        <h1>Mis tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
